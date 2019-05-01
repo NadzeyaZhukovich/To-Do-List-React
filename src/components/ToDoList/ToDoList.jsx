8df01e06fc8416d5fb74c990697a3f37f4aa09e6
Nadzeya <naduva0505@gmail.com>
2019-05-01 08:17:03 +0200

    Added base structure of the project; configuration webpack

---
+++ src/components/ToDoList/ToDoList.jsx
@@ -0,0 +1,14 @@
+import './ToDoList.scss';
+
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+export class ToDoList extends Component {
+   render(){
+       return (
+           <main className="toDoList">
+               
+           </main>
+       )
+   } 
+}
